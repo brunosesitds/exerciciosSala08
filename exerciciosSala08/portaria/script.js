a358b08eb756f88ce38708fa74391f2ec7420821
brunosesitds <brunosesitds@gmail.com>
2026-05-07 07:56:14 -0300

    Add files via upload

---
+++ exerciciosSala08/portaria/script.js
@@ -0,0 +1,6 @@
+function check() {
+    var wasborn = parseInt(document.getElementById('birth').value);
+    var res = document.getElementById('result');
+    var clientAge = document.getElementById('age');
+
+}
