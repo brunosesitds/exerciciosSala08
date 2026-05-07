6cd1844aac610300c5380068c4d4e21b90d8f241
brunosesitds <brunosesitds@gmail.com>
2026-05-07 11:09:56 -0300

    Add files via upload

--- exerciciosSala08/portaria/script.js
+++ exerciciosSala08/portaria/script.js
@@ -3,4 +3,26 @@ function check() {
     var res = document.getElementById('result');
     var clientAge = document.getElementById('age');
 
+    if(isNaN(wasborn)){
+        res.textContent = 'Type only numbers';
+        res.style.color = 'red';
+        return;
+    }
+    const thisYear = new Date().getFullYear();
+    const age = thisYear - wasborn;
+
+    if(age < 18){
+        res.textContent = 'Sai fora pai!';
+        res.style.color = 'red';
+        clientAge.textContent = `Your age is ${age}`;
+        document.getElementById('v1').style.display = 'block';
+        document.getElementById('v2').style.display = 'none';
+
+    } else {
+        res.textContent = 'Pode entrar filho!';
+        res.style.color = 'green';
+        clientAge.textContent = `Your age is ${age}`;
+        document.getElementById('v1').style.display = 'none';
+        document.getElementById('v2').style.display = 'block';
+    }
 }
